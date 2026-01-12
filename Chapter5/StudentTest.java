78e384e6aea1d10fed20feb3c6fdb5ffed2bfc1d
KimGyeonkang <v.tang3203@gmail.com>
2026-01-12 18:05:43 +0900

    Day6

--- Chapter5/StudentTest.java
+++ Chapter5/StudentTest.java
@@ -8,5 +8,28 @@ public class StudentTest {
         System.out.println(studentAhn.studentName); // 클래스의 멤버 변수 출력
         System.out.println(studentAhn.getStudentName()); // 같은 클래스 내 메서드 호출
         // 만일 사용할 클래스가 다른 패키지에 있다면, import문으로 해당 클래스를 불러와야 함.
+
+
+
+        Student1 studentLee = new Student1(1001, "Lee");
+        // Cannot find symbol 문제 해결. 방법은 Student1 클래스 내에 주석 달아둠.
+
+        studentLee.setKoreanSubject("국어", 100);
+        studentLee.setMathSubject("수학", 50);
+
+        Student1 studentKim = new Student1(1002, "Kim");
+
+        studentKim.setKoreanSubject("국어", 70);
+        studentKim.setMathSubject("수학", 85);
+
+        studentLee.showStudentInfo();
+        studentKim.showStudentInfo();
+
+
+        Student studentLee = new Student();
+        //studentLee.studentName = "이상원";
+        studentLee.setStudentName("이상원");
+
+        System.out.println(studentLee.getStudentName());
     }
 }
---
+++ Chapter5/Subject.java
@@ -0,0 +1,6 @@
+package Chapter5;
+// 학생 클래스의 과목 정보를, 별도의 과목 클래스로 분리하기
+public class Subject {
+    String SubjectName; // 과목명
+    int scorePoint; // 과목 점수
+}
